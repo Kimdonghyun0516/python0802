cc23e14931f256fc5b5256e21c17e8b79cb25a9a
Kimdonghyun <70845889+Kimdonghyun0516@users.noreply.github.com>
2021-08-03 16:54:25 +0900

    디버깅 연습0803

--- inClass/# Function1.py
+++ inClass/# Function1.py
@@ -17,3 +17,16 @@ def swap(x,y):
 # 호출
 result = swap(3,4)
 print(result)
+
+# 디버깅 연습
+def intersect(prelist, postlist):
+    # 지역변수
+    result = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+
+# 호출
+print( intersect("HAM", "SPAM") )
